3dd9af01e586d9f0bb826f39d3d8d2813b6f53bd
henriqueGJesus <hj30032006@gmail.com>
2024-05-09 16:53:31 -0300

    feat: projeto comentario

--- api/src/main/java/og/net/api/repository/ValorPropriedadeTarefa.java
+++
@@ -1,8 +0,0 @@
-package og.net.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ValorPropriedadeTarefa extends JpaRepository<ValorPropriedadeTarefa,Long> {
-}
