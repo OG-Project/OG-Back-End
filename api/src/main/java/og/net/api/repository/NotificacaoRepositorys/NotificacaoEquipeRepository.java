3dce208b11d19859292f2eec0b4da5ee5df3866f
Davi-Simplicio <davi31102005@gmail.com>
2024-05-14 20:58:42 -0300

    merge

--- api/src/main/java/og/net/api/repository/NotificacaoRepositorys/NotificacaoEquipeRepository.java
+++ api/src/main/java/og/net/api/repository/NotificacaoRepositorys/NotificacaoEquipeRepository.java
@@ -1,9 +1,14 @@
 package og.net.api.repository.NotificacaoRepositorys;
 
+import og.net.api.model.entity.Equipe;
 import og.net.api.model.entity.Notificacao.NotificacaoEquipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NotificacaoEquipeRepository  extends JpaRepository<NotificacaoEquipe, Integer> {
+
+    List<NotificacaoEquipe> findAllByEquipe(Equipe equipe);
 }
