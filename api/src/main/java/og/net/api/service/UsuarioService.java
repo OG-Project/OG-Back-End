adec0f56cad7fa320eee36a8b94cc54ea0d2a96f
henriqueGJesus <hj30032006@gmail.com>
2024-05-21 11:35:25 -0300

    feat: send email errado

--- api/src/main/java/og/net/api/service/UsuarioService.java
+++ api/src/main/java/og/net/api/service/UsuarioService.java
@@ -17,11 +17,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 @Service
 @AllArgsConstructor
@@ -31,12 +36,13 @@ public class UsuarioService {
     private EquipeService equipeService;
     private UsuarioProjetoRepository usuarioProjetoRepository;
     private UsuarioAceitoRepository usuarioAceitoRepository;
-    private ComentarioRepository  comentarioRepository;
+    private ComentarioRepository comentarioRepository;
     private UsuarioTarefaRepository usuarioTarefaRepository;
     private EquipeUsuarioRepository equipeUsuarioRepository;
     private ModelMapper modelMapper;
     private final UsuarioDetailsEntityRepository usuarioDetailsEntityRepository;
     private final PasswordEncoder passwordEncoder;
+
     public Usuario buscarUm(Integer id) {
         return usuarioRepository.findById(id).get();
     }
@@ -46,20 +52,73 @@ public class UsuarioService {
     }
 
     public void deletar(Integer id) {
-        Usuario usuario=usuarioRepository.findById(id).get();
+        Usuario usuario = usuarioRepository.findById(id).get();
         System.out.println(usuario);
         comentarioRepository.deleteAllByAutor_Id(usuario.getId());
         usuarioAceitoRepository.deleteAllByUsuario_Id(id);
 
         usuarioProjetoRepository.deleteByIdResponsavel(id);
-        for (UsuarioTarefa tarefa: usuario.getTarefas()){
+        for (UsuarioTarefa tarefa : usuario.getTarefas()) {
             usuarioTarefaRepository.deleteByTarefa_Id(tarefa.getTarefa().getId());
         }
 
-        for (EquipeUsuario equipe:usuario.getEquipes()){
+        for (EquipeUsuario equipe : usuario.getEquipes()) {
             equipeUsuarioRepository.deleteByEquipe(equipe.getEquipe());
         }
         usuarioRepository.deleteById(id);
+    }
+
+    public void enviaEmail(Usuario usuario) {
+        try {
+            Properties properties = System.getProperties();
+
+            // define os dados básicos
+            String host = "smtp.gmail.com";
+            String email = "og.project.weg@gmail.com";
+            String password = "sryx rozm wixy iool" ;
+
+            //liga os protocolo tudo
+            properties.put("mail.smtp.auth", "true");
+            properties.put("mail.smtp.starttls.enable", "true");
+            properties.put("mail.smtp.host", host);
+            properties.put("mail.smtp.port", "587");
+            properties.put("mail.smtp.ssl.trust", host);
+
+            Session session = Session.getDefaultInstance(properties, new Authenticator() {
+                protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(email, password);
+                }
+            });
+
+            try {
+                MimeMessage message = new MimeMessage(session);
+                message.setFrom(new InternetAddress(email, "OG TESTE MLK"));
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(usuario.getEmail()));
+                message.setSubject("Confirme Seu Email!");
+
+                String htmlContent = "<html><body style='background-color: #f6f8fa;'>" +
+                        "<div style='margin: 0 auto; max-width: 600px; padding: 20px; text-align: center;'>" +
+                        "<img src='api/src/main/resources/logo.jpg' " +
+                        "alt='OG LOGO' style='margin-bottom: 20px; width: 140px; height: auto;'>" +
+                        "<div style='background-color: #fff; border-radius: 6px; padding: 40px;'>" +
+                        "<p style='font-size: 14px; color: #586069;'>" + usuario.getUsername() + " </p>" +
+                        "<h1 style='font-size: 36px; color: #092C4C; margin-top: 10px; margin-bottom: 30px;'>  </h1>" +
+                        "<h2 style='color: #092C4C; font-weight: 500;'><a href='http://localhost:8082" + usuario.getEmail() + "'>Confirmar E-mail</a></h2>" +
+                        "</div>" +
+                        "</div>" +
+                        "</body></html>";
+
+                message.setContent(htmlContent, "text/html; charset=utf-8");
+
+                Transport.send(message);
+            } catch (MessagingException mex) {
+                mex.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (Exception e) {
+            System.out.println("Falha E-mail");
+        }
     }
 
     public Usuario cadastrar(IDTO dto) throws IOException, DadosIncompletosException {
@@ -90,6 +149,7 @@ public class UsuarioService {
         usuario.setSenha(passwordEncoder.encode(usuarioCadastroDTO.getSenha()));
         try {
             usuarioRepository.save(usuario);
+//            enviaEmail(usuario);
         } catch (Exception e) {
             throw new DadosIncompletosException();
         }
--- api/src/main/java/og/net/api/webSocket/WebSocketControllerChat.java
+++ api/src/main/java/og/net/api/webSocket/WebSocketControllerChat.java
@@ -1,10 +1,35 @@
 package og.net.api.webSocket;
 
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class WebSocketControllerChat extends AbstractWebSocketHandler {
 
 
+    private final Set<WebSocketSession> sessions = new HashSet<>();
 
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
 
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        session.sendMessage(message);
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        sessions.remove(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+        sessions.remove(session);
+    }
 }
