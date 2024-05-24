f808787759631f3f8cde806d734e69d612337f73
WesleyDIO <wesley_d_ferreira@estudante.sesisenai.org.br>
2024-05-23 21:59:48 -0300

    merge

--- api/src/main/java/og/net/api/webSocket/WebSocketConfig.java
+++ api/src/main/java/og/net/api/webSocket/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
         registry.addHandler(new WebSocketControllerProjeto(), "/og/webSocket/projeto/{id}").setAllowedOrigins("*");
         registry.addHandler(new WebSocketControllerNotificacao(), "/og/webSocket/convite/{id}").setAllowedOrigins("*");
         registry.addHandler(new WebSocketControllerNotificacao(), "/og/webSocket/notificacao/{id}").setAllowedOrigins("*");
-        registry.addHandler(new WebSocketControllerChat(), "/og/webSocket/chat").setAllowedOrigins("*");
+        registry.addHandler(new WebSocketControllerChat(), "/og/webSocket/chat/1").setAllowedOrigins("*");
 
     }
 }
